3ee979b44ac6b688e637eeaf1cb84212f6ce2f3b
Sam <sam-rez@users.noreply.github.com>
2026-01-17 22:55:57 -0800

    Ran ingest file

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 def main():
     loader = TextLoader("docs/resume.txt")
